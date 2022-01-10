06a815f8517a6eb2214e81e68a4f7e5e4c17fa25
mark <markpackham1@gmail.com>
2022-01-10 14:19:14 +0000

    add flush method so both create queries can be run at same time
    https://youtu.be/_EUr93OWB68?t=492

--- src/DataFixtures/MovieFixtures.php
+++ src/DataFixtures/MovieFixtures.php
@@ -15,5 +15,15 @@ class MovieFixtures extends Fixture
         $movie->setReleaseYear(2008);
         $movie->setDescription('Dark Knight description...');
         $movie->setImagePath('https://cdn.pixabay.com/photo/2021/06/18/11/22/batman-6345897_960_720.jpg');
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2->setTitle('Captain America');
+        $movie2->setReleaseYear(2011);
+        $movie2->setDescription('Captain America description...');
+        $movie2->setImagePath('https://pixabay.com/illustrations/captain-america-avengers-marvel-5692937/');
+        $manager->persist($movie2);
+
+        $manager->flush();
     }
 }
