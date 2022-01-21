b607470e6b57e247e740cecddac3e7478a2624e8
mark <markpackham1@gmail.com>
2022-01-21 15:50:49 +0000

    copy image files from asserts to public
    https://youtu.be/ek1TCYqAvbg?t=358

--- webpack.config.js
+++ webpack.config.js
@@ -60,6 +60,12 @@ Encore
     options.postcssOptions = {
       config: "./postcss.config.js",
     };
+  })
+
+  .copyFiles({
+    from: "./assets/images",
+    to: "images/[path][name].[hash:8].[ext]",
+    pattern: /\.(png\jpg\jpeg)$/,
   });
 
 // enables Sass/SCSS support
