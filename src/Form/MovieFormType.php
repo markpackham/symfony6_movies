c883af29d9803b4a43f50e788385ff0094767a5b
mark <markpackham1@gmail.com>
2022-02-08 16:39:03 +0000

    style title field in the Form Type
    https://youtu.be/vbT7tGwCRUM?t=841

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -6,13 +6,20 @@ use App\Entity\Movie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MovieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full text-6xl outline-none',
+                    'placeholder' => 'Enter title'
+                ),
+                'label' => false
+            ])
             ->add('releaseYear')
             ->add('description')
             ->add('imagePath')
