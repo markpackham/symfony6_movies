ab41bb7bc5f4c23117cadfb332ba065469593618
mark <markpackham1@gmail.com>
2022-02-14 12:43:23 +0000

    adding validation checks
    https://youtu.be/2LKv_MZo_-k?t=198

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -6,6 +6,7 @@ use App\Repository\MovieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MovieRepository::class)
@@ -21,16 +22,20 @@ class Movie
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Length(min=3)
      */
     private $title;
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\NotBlank
      */
     private $releaseYear;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank
      */
     private $description;
 
